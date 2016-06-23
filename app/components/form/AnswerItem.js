62c42713d2b1d7161bd0fc8a5b66be6acc84d239
Theo Kleman <theo@kleman.fr>
2016-06-23 02:20:28 +0200

    WIP form questions/answers structure

---
+++ app/components/form/AnswerItem.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default class AnswerItem extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        var char = ""
+        switch(this.props.answerId) {
+            case 1:
+                char = "A"
+                break;
+            case 2:
+                char = "B"
+                break;
+            case 3:
+                char = "C"
+                break;
+            case 4:
+                char = "D"
+                break;
+        }
+
+        return(
+            <div
+                className="form--answers--item"
+                ref={this.props.answerId}
+                onClick={this.props.goToNextStep.bind(this)}
+                >
+                <button className="key">
+                    <span className="key--content">{char}</span>
+                    <span className="key--double"></span>
+                </button>
+                <div className="item--answer">
+                    <p>{this.props.answerText}</p>
+                </div>
+            </div>
+        );
+    }
+}
