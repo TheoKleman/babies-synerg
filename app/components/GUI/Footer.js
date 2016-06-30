e5eb660933f2b8517bf359aa390d6078e6a5e800
stephenrichard <stephen.richard44@gmail.com>
2016-06-30 16:58:24 +0200

    Multiple fixs (synger logo over sprite / sort button translation...

--- app/components/GUI/Footer.js
+++ app/components/GUI/Footer.js
@@ -1,17 +1,59 @@
 import React from 'react'
 import Draggable from 'react-draggable'
+import SpriteAnimator from 'react-sprite-animator'
 
 export default class Footer extends React.Component {
     constructor() {
         super()
+
+        this.state = {
+            showNyan: false
+        }
+
+        this.nyanAudio = document.createElement('audio');
+        this.nyanAudio.src = './media/nyancat.wav'
+        this.nyanAudio.volume = 0.4
+    }
+
+    handleMouseEnter(e) {
+        this.nyanAudio.currentTime = 0
+        this.nyanAudio.play()
+        this.setState({
+            showNyan: true
+        })
+    }
+
+    handleMouseLeave(e) {
+        this.nyanAudio.pause()
+        this.setState({
+            showNyan: false
+        })
     }
 
     render() {
+
+        if(this.state.showNyan) {
+            var nyanCat = <SpriteAnimator
+                        ref="bodyAnimation"
+                        className="nyan-sprite"
+                        sprite="images/animation-nyan.png"
+                        width={360}
+                        height={360}
+                        timeout={80}
+                        shouldAnimate={true} />
+        }
+
         return(
             <footer className="main-footer">
-                <div className="logo">
+                <a 
+                    onMouseEnter={this.handleMouseEnter.bind(this)}
+                    onMouseLeave={this.handleMouseLeave.bind(this)}
+                    href="http://www.synerghetic.net/"
+                    className="logo"
+                    target="_blank">
                     <img src="/images/logo.png" alt=""/>
-                </div>
+                </a>
+                {nyanCat}
                 <div className="misc">
                     <div className="misc--buttons">
                         <a 
